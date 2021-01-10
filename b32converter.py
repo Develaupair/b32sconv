b0f60c849ff9114461540f6d008364b8a60f042f
Develaupair <develaupair@protonmail.com>
2021-01-10 21:14:21 +0100

    fixed small bug with quotation marks

--- b32converter.py
+++ b32converter.py
@@ -1,17 +1,14 @@
 """
 This program is a fun text to base 32 number converter. This works by interpreting every character (0-9a-v) as digit of a 32 digit alphabet and calculating the number. It also works the other way around.
 Copyright (C) 2021 Anubosiris
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
 by the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
-
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
@@ -35,7 +32,7 @@ if __name__ == "__main__":
             print(decNumberToBaseNString(number))
         except:
             try:
-                print(baseNStringToDecNumber(str(sys.argv[1])))
+                print(baseNStringToDecNumber(str(sys.argv[1].strip("\""))))
             except:
                 print("unknown error")
                 exit(1)
